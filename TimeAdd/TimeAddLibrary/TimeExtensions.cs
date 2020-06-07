bc6229cb4ebb0b90718816d1b735d0ae75813693
Azriel Hoh <azriel91@gmail.com>
2020-06-07 12:51:48 +1200

    Adds `TimeAdd` solution with `TimeAddLibrary` library.

---
+++ TimeAdd/TimeAddLibrary/TimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TimeAddLibrary
+{
+    public static class TimeExtensions
+    {
+        public static TimeSpan Secs(this Int32 seconds) =>
+            new TimeSpan(0, 0, seconds);
+
+        public static TimeSpan Mins(this Int32 minutes) =>
+            new TimeSpan(0, minutes, 0);
+
+        public static TimeSpan Hours(this Int32 hours) =>
+            new TimeSpan(hours, 0, 0);
+
+        public static String HumanReadable(this TimeSpan timeSpan)
+        {
+            return $"{timeSpan.Hours} hours {timeSpan.Minutes} minutes " +
+            "{timeSpan.Seconds} seconds";
+        }
+    }
+}
