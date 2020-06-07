6c26d225d5f85b0c561f2913179e599fb878613b
Azriel Hoh <azriel91@gmail.com>
2020-06-07 14:18:27 +1200

    Adds `Lambdas` app.

---
+++ Lambdas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Lambdas;
+
+namespace Lambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = (a, b) => a + b;
+            Console.WriteLine($"add: {add(1, 2)}");
+
+            int x = 10;
+            Func<int, int> capture = a => a + x;
+            Console.WriteLine($"capture: {capture(1)}");
+
+            Value v = new Value(10);
+            Action mutateValue = () => v.inner += 1;
+            mutateValue();
+            mutateValue();
+
+            Console.WriteLine($"value: {v.inner}");
+
+            // Oh no, multiple lambdas have mutable access!
+            Action mutateValue2 = () => v.inner += 1;
+            mutateValue2();
+            mutateValue2();
+
+            Console.WriteLine($"value: {v.inner}");
+        }
+    }
+}
---
+++ Lambdas/Value.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Lambdas
+{
+    public class Value
+    {
+        public Value(Int64 inner) => this.inner = inner;
+        public Int64 inner { get; set; }
+    }
+}
